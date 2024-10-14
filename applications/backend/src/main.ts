79d830ac66dbe4221ff9d028e6a9167f42c6b651
Rahul <rahulnpre@gmail.com>
2024-10-13 22:02:57 -0400

    Ports on nextjs changed

--- applications/backend/src/main.ts
+++ applications/backend/src/main.ts
@@ -6,12 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3001',  // Replace with the Next.js frontend URL
+    origin: 'http://localhost:4000', // Replace with the Next.js frontend URL
     credentials: true,
   };
-  
+
   app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
-
